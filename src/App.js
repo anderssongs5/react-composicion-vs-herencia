68ea413cc75cbf55d9d72fae4d9df0418470de42
Andersson García Sotelo <agarcias@sura.com.co>
2019-11-07 22:01:27 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class ButtonHerencia extends Component {
+  constructor( props) {
+    super(props);
+    this.borderColor = '#09f'
+  }
+
+  render() {
+    return(
+      <button style={{ borderColor: this.borderColor, display: 'block'}}>
+        {this.props.label}
+      </button>
+    )
+  }
+}
+
+class ButtonPeligro extends ButtonHerencia {
+  constructor(props) {
+    super(props)
+    this.borderColor = 'red'
+  }
+}
+
+class ButtonConLeyenda extends ButtonHerencia {
+  render() {
+    return (
+      <div>
+        {super.render()}
+        <small>{this.props.legend}</small>
+      </div>
+    )
+  }
+}
+
+class ButtonComposicion extends Component {
+  render() {
+    return(
+      <button style={{ borderColor: this.props.borderColor, display: 'block'}}>
+        {this.props.label}
+      </button>
+    )
+  }
+}
+
+class ButtonPeligroComposicion extends Component {
+  render() {
+    return (
+      <ButtonComposicion label={this.props.label} borderColor='red' />
+    )
+  }
+}
+
+class ButtonConLeyendaComposicion extends Component {
+  render() {
+    return (
+      <div>
+        <ButtonComposicion label = {this.props.label} borderColor={this.props.borderColor} />
+        <small>{this.props.legend}</small>
+      </div>
+    )
+  }
+}
+
+class App extends Component{
+ render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Composición vs Herencia</h4>
+      <ButtonHerencia label='Click aquí herencia!!!' />
+      <br />
+      <ButtonPeligro label = 'Click aquí herencia!!! Peligro' />
+      <br />
+      <ButtonConLeyenda legend = 'Leyenda' label = 'Click aquí herencia!!! Leyenda' />
+      <br/><br/><br/>
+      <ButtonComposicion label='Click aquí composicion!!!' />
+      <br />
+      <ButtonPeligroComposicion label = 'Click aquí composicion!!! Peligro' />
+      <br />
+      <ButtonConLeyendaComposicion legend = 'Leyenda' label = 'Click aquí composicion!!! Leyenda' />
     </div>
   );
+ }
 }
 
 export default App;
